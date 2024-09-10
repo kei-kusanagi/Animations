cdaa977c8b6a3f63e992c49f9bb98f0fee2b31f3
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-09-10 15:40:25 -0600

    Personalización de animaciones en SwiftUI

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -12,14 +12,34 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Button("Tap me"){
-                animationAmount += 1
+//                animationAmount += 1
             }.padding(50)
                 .background(.red)
                 .foregroundStyle(.white)
                 .clipShape(.circle)
-                .scaleEffect(animationAmount)
-                .blur(radius: (animationAmount - 1 ) * 1.05)
-                .animation(.default, value: animationAmount)
+//                .scaleEffect(animationAmount)
+//                .blur(radius: (animationAmount - 1 ) * 1.05)
+//
+//                .animation(.default, value: animationAmount)
+//                .animation(.spring(duration: 1, bounce: 0.9), value: animationAmount)
+//                .animation(.easeInOut(duration: 2), value: animationAmount)
+                .overlay(
+                Circle()
+                    .stroke(.red)
+                    .scaleEffect(animationAmount)
+                    .opacity(2-animationAmount)
+                    .animation(
+                        .easeOut(duration: 1)
+    //                    .delay(1)
+    //                        .repeatCount(3, autoreverses: true)
+                            .repeatForever(autoreverses: false)
+                        ,value: animationAmount
+                    )
+                )
+                .onAppear{
+                    animationAmount = 2
+                }
+
         }
         .padding()
     }
