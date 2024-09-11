25342eb8f78792141671ad8ede7ba3970e35ab42
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-09-11 16:32:01 -0600

    Mostrar y ocultar vistas con transiciones

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,49 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    @State private var dragAmount = CGSize.zero
-    let letters = Array("Hello SwiftUI")
-    @State private var enabled = false
-    
+    @State private var isShowingRed = false
     
     var body: some View {
-        LinearGradient(colors: enabled ? [.indigo, .green, .blue] : [.yellow, .red, .orange], startPoint: .topLeading, endPoint: .bottomTrailing)
-            .frame(width: 300, height: 200)
-            .clipShape(.rect(cornerRadius: 10))
-            .offset(dragAmount)
-            .gesture(
-                DragGesture()
-                    .onChanged{dragAmount = $0.translation}
-                    .onEnded{ _ in
-                        withAnimation(.bouncy){
-                            dragAmount = .zero
-                            enabled.toggle()
-                        }
-                    }
-            )
-        
-        
-        HStack(spacing: 0) {
-            ForEach(0..<letters.count, id: \.self) { num in
-                Text(String(letters[num]))
-                    .padding(5)
-                    .font(.title)
-                    .background(enabled ? .blue : .red)
-                    .offset(dragAmount)
-                    .animation(.linear.delay(Double(num) / 20), value: dragAmount)
-            }
-        }
-        .gesture(
-            DragGesture()
-                .onChanged { dragAmount = $0.translation }
-                .onEnded { _ in
-                    dragAmount = .zero
-                    enabled.toggle()
+        VStack {
+            Button("Tap Me"){
+                withAnimation {
+                    isShowingRed.toggle()
                 }
-        )
+            }
+            if isShowingRed{
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+//                    .transition(.scale)
+                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+            }}
     }
-}
+    }
 
 
 
